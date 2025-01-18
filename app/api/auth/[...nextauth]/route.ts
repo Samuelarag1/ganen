2f8f97d70de34e728098edc3733d1856c53b45e8
Samuel <samuelaragon616@gmail.com>
2025-01-18 16:53:41 -0300

    Update NextAuth secret environment variable to NEXTAUTH_SECRET

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -24,7 +24,7 @@ const handler = NextAuth({
   pages: {
     signIn: "/auth/signin",
   },
-  secret: process.env.SECRET!,
+  secret: process.env.NEXTAUTH_SECRET!,
 });
 
 export { handler as GET, handler as POST };
